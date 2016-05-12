d9a19dba054cb52407c5baad96daab9d47398039
dspak <daniel.spakowicz@gmail.com>
2016-05-12 15:03:41 -0400

    point to polys attempt

--- scf_cleaning.R
+++ scf_cleaning.R
@@ -15,39 +15,70 @@
 # load required packages
 #install.packages("maptools")
 #install.packages("ggmap")
+#install.packages("rgdal")
+#install.packages("spatialEco")
 library(maptools)
 library(RColorBrewer)
 library(ggmap)
 library(ggplot2)
+library(rgdal)
+library(plyr)
+library(rgeos)
+library(spatialEco)
 
-# input dir
+# base working directory
+setwd("/Users/danielspakowicz/Box Sync/projects/scfbw/")
 
 # figs output dir
 figout <- "/Users/danielspakowicz/Box Sync/projects/scfbw/figures"
 
 # load issues file
-scf_issues <- "~/Box Sync/projects/scfbw/scf_data/nh_scf_issues.csv"
+scf_issues <- "raw_data/scf_data/nh_scf_issues.csv"
 df <- read.csv(scf_issues)
+
+# make a column of just year
 df$created_at <- as.POSIXct(df$created_at)
 df$year <- strftime(df$created_at, format = "%Y")
 
-
-
-
-# check for extraneous values
-
-
-
+# Figure of posts by year  
+ggplot(data = df)+
+  geom_point(aes(x = lng, y = lat, color = year), size=0.3)+
+  #   geom_line(data=area.points, 
+  #                aes(x=long, y = lat, fill = NULL))+
+  ylim(41.25, 41.35)+
+  xlim(-73, -72.85)+ 
+  facet_wrap(~year)
+ggsave("scf_posts_by_year.pdf", path = figout)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~#~~~~~~~~~~~~~~~~~~~~~~~~~~~#~~~~~~~~~~~~~~~~~~~~~~~~~~~#~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Apply shape files to scf data
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~#~~~~~~~~~~~~~~~~~~~~~~~~~~~#~~~~~~~~~~~~~~~~~~~~~~~~~~~#~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# load in shape file
-shape.file <- "~/Box Sync/projects/scfbw/shape_files/blockgroupct_37800_0000_2010_s100_census_1_shp/wgs84/blockgroupct_37800_0000_2010_s100_census_1_shp_wgs84.shp"
-area <- readShapePoly(shape.file)
+
+# view shape file
+shape.dir <- "shape_files/Weaver/nh_neighborhoods/"
+shape.file <- "nh_neighborhoods.shp"
+area <- readShapePoly(paste(shape.dir, shape.file, sep = ""))
 plot(area)
+
+# load full set of shape files
+neighborhoods <- readOGR(dsn=path.expand("shape_files/Weaver/nh_neighborhoods/"),
+                 layer="nh_neighborhoods")
+
+# create dataframe from shape file
+neighborhoods@data$id = rownames(neighborhoods@data)
+neighborhoods.points = fortify(neighborhoods, region="id")
+neighborhoods.df = join(neighborhoods.points, neighborhoods@data, by="id")
+
+# convert df into SpatialPoints DataFrame
+coordinates(df) = c("lng", "lat")
+
+# this step fails, check http://r-sig-geo.2731867.n2.nabble.com/point-in-polygon-or-over-help-td7583635.html
+scf.in.neighborhoods <- point.in.poly(df, neighborhoods)
+
+
+
 
 # convert shape file into dataframe
 area.points <- fortify(area)
@@ -93,15 +124,7 @@ ggmap(mapImage) +
   labs(x = "Longitude",
        y = "Latitude")
 
-# Figure of posts by year  
-ggplot(data = df)+
-  geom_point(aes(x = lng, y = lat, color = year), size=0.3)+
-#   geom_line(data=area.points, 
-#                aes(x=long, y = lat, fill = NULL))+
-  ylim(41.25, 41.35)+
-  xlim(-73, -72.85)+ 
-  facet_wrap(~year)+
-  ggsave("scf_posts_by_year.pdf", path = figout)
+
 
 # Figure of posts by year  
 ggplot(data = df)+
@@ -116,3 +139,30 @@ ggplot(data = df)+
 
 
 # create a column for number of watchers per issue
+
+
+# Trying with another set of packages
+# http://gis.stackexchange.com/questions/133625/checking-if-points-fall-within-polygon-shapefile
+
+
+data(meuse)
+xy = meuse[c("x", "y")] # retrieve coordinates as data.frame
+class(meuse)
+data(meuse) # reload data.frame
+coordinates(meuse) = c("x", "y") # specify column names
+class(meuse)
+data(meuse) # reload data.frame
+coordinates(meuse) = c(1, 2) # specify column names
+class(meuse)
+data(meuse) # reload data.frame
+coordinates(meuse) = ~x+y # formula
+class(meuse)
+data(meuse) # reload data.frame
+coordinates(meuse) = xy   # as data frame
+class(meuse)
+data(meuse) # reload data.frame
+coordinates(meuse) = as.matrix(xy)   # as matrix
+meuse$log.zn = log(meuse$zinc)
+class(meuse)
+dim(meuse)
+
